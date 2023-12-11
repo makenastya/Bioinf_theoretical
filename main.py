f116521d4a29188c084cf64af33d02f216b11eb5
anastasiamakeeva <nastya.mak.2002@mail.ru>
2023-12-11 21:41:17 +0300

    Reading fasta files

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-
+f1 = open('1.fa', 'r')
+f2 = open('2.fa', 'r')
+for line in f1:
+    if line[0] == '>':
+        name1 = line[1:]
+    else:
+        s1 = line
+for line in f2:
+    if line[0] == '>':
+        name2 = line[1:]
+    else:
+        s2 = line
